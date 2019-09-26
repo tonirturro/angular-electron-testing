440fc07c619f8ec6a09810acc52888390450d98a
Garcia <a.garcia@hp.com>
2019-09-26 12:16:53 +0200

    Use spy components

--- src/frontend/App/Components/main-page.component.spec.ts
+++ src/frontend/App/Components/main-page.component.spec.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, DebugElement, EventEmitter, Input, Output } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { StateService } from "@uirouter/angular";
 import { of, Subject } from "rxjs";
 import { configureTestSuite } from "../../../../test/configureTestSuite";
@@ -23,17 +24,39 @@ class ToolFakeBarComponent {
     @Output() public onEditPages = new EventEmitter<any>();
     @Output() public onSettings = new EventEmitter<any>();
     @Output() public onClose = new EventEmitter<any>();
+
+    public addDevice() {
+        this.onAddDevice.emit();
+    }
+    public editDevice() {
+        this.onEditDevices.emit();
+    }
+    public editPages() {
+        this.onEditPages.emit();
+    }
+    public settings() {
+        this.onSettings.emit();
+    }
+    public close() {
+        this.onClose.emit();
+    }
 }
 @Component({ selector: "device-panel", template: ``})
 class DevicePanelFakeComponent {
-    @Input() public devices: [];
+    @Input() public devices: IDevice[];
     @Input() public selectedDeviceId: number;
     @Output() public onDeleteDevice = new EventEmitter<number>();
     @Output() public onSelectedDevice = new EventEmitter<number>();
+
+    public deleteDevice(id: number) {
+        this.onDeleteDevice.emit(id);
+    }
+    public selectDevice(id: number) {
+        this.onSelectedDevice.emit(id);
+    }
 }
 @Component({ selector: "ui-view", template: ``})
-class UiViewFakeComponent {
-}
+class UiViewFakeComponent {}
 // tslint:disable-next-line: no-empty
 function NOOP() {}
 
@@ -41,15 +64,27 @@ describe("Given a main page component", () => {
 
     configureTestSuite();
 
-    const devices: IDevice[] = [{
-        id: 1,
-        name: "Device 2"
-    }];
+    const devices: IDevice[] = [
+        {
+            id: 1,
+            name: "Device 1"
+        },
+        {
+            id: 2,
+            name: "Device 2"
+        },
+        {
+            id: 3,
+            name: "Device 3"
+        }
+    ];
     const languageObservable: Subject<any> = new Subject<any>();
 
     let fixture: ComponentFixture<MainPageComponent>;
-    let element: Element;
+    let element: DebugElement;
     let component: MainPageComponent;
+    let toolbar: ToolFakeBarComponent;
+    let devicePanel: DevicePanelFakeComponent;
     let goSpy: jasmine.Spy;
     let setLanguageSpy: jasmine.Spy;
     let deviceNameSpy: jasmine.Spy;
@@ -81,8 +116,12 @@ describe("Given a main page component", () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(MainPageComponent);
-        element = fixture.nativeElement;
+        element = fixture.debugElement;
         component = fixture.componentInstance;
+        const devicePanelElement = element.query(By.directive(DevicePanelFakeComponent));
+        devicePanel = devicePanelElement.componentInstance as DevicePanelFakeComponent;
+        const toolbarElememt = element.query(By.directive(ToolFakeBarComponent));
+        toolbar = toolbarElememt.componentInstance as ToolFakeBarComponent;
     });
 
     beforeEach(() => {
@@ -109,11 +148,13 @@ describe("Given a main page component", () => {
     });
 
     it("When it is initialized Then the html is defined", () => {
-        expect(element).toBeDefined();
+        expect(element.nativeElement).toBeTruthy();
+        expect(devicePanel).toBeTruthy();
+        expect(toolbar).toBeTruthy();
     });
 
-    it("When is ititialized Then it gets the existing devices", () => {
-        expect(component.devices).toEqual(devices);
+    it("When is inititialized Then the device panel gets the existing devices", () => {
+        expect(devicePanel.devices).toEqual(devices);
     });
 
     it("When it is initialized Then it goes to the pages edition", () => {
@@ -128,34 +169,46 @@ describe("Given a main page component", () => {
 
     it("When calling edit devices Then the view changes to the device edition", () => {
         component.selectedDeviceId = devices[0].id;
+        fixture.detectChanges();
         const expectedDeviceSelection: IDeviceSelection = { deviceId: component.selectedDeviceId };
-        component.editDevices();
 
-        expect(component.editingDevices).toBeTruthy();
+        toolbar.editDevice();
+        fixture.detectChanges();
+
+        expect(toolbar.editingDevices).toBeTruthy();
         expect(goSpy).toHaveBeenCalledWith("device", expectedDeviceSelection);
     });
 
     it("When calling edit pages Then the view changes to the pages edition", () => {
         component.selectedDeviceId = devices[0].id;
+        fixture.detectChanges();
         const expectedDeviceSelection: IDeviceSelection = { deviceId: component.selectedDeviceId };
-        component.editPages();
+
+        toolbar.editPages();
+        fixture.detectChanges();
 
         expect(component.editingDevices).toBeFalsy();
         expect(goSpy).toHaveBeenCalledWith("pages", expectedDeviceSelection);
     });
 
     it("When selecting device Then it is reflected by the associated property", () => {
-        const ExpectedDeviceId = 5;
-        component.selectDevice(ExpectedDeviceId);
-        expect(component.selectedDeviceId).toBe(ExpectedDeviceId);
+        const ExpectedDeviceId = devices[1].id;
+
+        devicePanel.selectDevice(ExpectedDeviceId);
+        fixture.detectChanges();
+
+        expect(devicePanel.selectedDeviceId).toBe(ExpectedDeviceId);
     });
 
     it("When selecting device Then the view is adjusted with the selected device", () => {
-        const ExpectedDeviceId = 8;
+        const ExpectedDeviceId =  devices[2].id;
         component.editingDevices = true;
-        component.selectDevice(ExpectedDeviceId);
-        const expectedDeviceSelection: IDeviceSelection = { deviceId: component.selectedDeviceId };
+        fixture.detectChanges();
 
+        devicePanel.selectDevice(ExpectedDeviceId);
+        fixture.detectChanges();
+
+        const expectedDeviceSelection: IDeviceSelection = { deviceId: component.selectedDeviceId };
         expect(goSpy).toHaveBeenCalledWith("device", expectedDeviceSelection);
     });
 
@@ -163,7 +216,7 @@ describe("Given a main page component", () => {
         const expectedName = "DEVICE";
         deviceNameSpy.and.returnValue(of(expectedName));
 
-        component.addDevice();
+        toolbar.addDevice();
 
         expect(addNewDeviceSpy).toHaveBeenCalledWith(expectedName);
     });
@@ -174,7 +227,7 @@ describe("Given a main page component", () => {
         const expectedDialogMessage: IMessageParam = { message: `${deleteMessage}: ${devices[0].name}` };
         modalPushSpy.and.returnValue(of({}));
 
-        component.deleteDevice(devices[0].id);
+        devicePanel.deleteDevice(devices[0].id);
 
         expect(modalPushSpy).toHaveBeenCalledWith(EModals.Confimation, expectedDialogMessage);
     });
@@ -183,7 +236,7 @@ describe("Given a main page component", () => {
         deleteDeviceMessageSpy.and.returnValue(of(""));
         modalPushSpy.and.returnValue(of({}));
 
-        component.deleteDevice(devices[0].id);
+        devicePanel.deleteDevice(devices[0].id);
 
         expect(deleteDeviceSpy).toHaveBeenCalledWith(devices[0].id);
     });
@@ -193,8 +246,9 @@ describe("Given a main page component", () => {
         const observableMock = new Subject<any>();
         modalPushSpy.and.returnValue(observableMock);
 
-        component.deleteDevice(devices[0].id);
+        devicePanel.deleteDevice(devices[0].id);
         observableMock.error({});
+        fixture.detectChanges();
 
         expect(deleteDeviceSpy).not.toHaveBeenCalled();
     });
@@ -202,7 +256,7 @@ describe("Given a main page component", () => {
     it("When calling settings Then the settings dialog is open", () => {
         modalPushSpy.and.returnValue(of({}));
 
-        component.settings();
+        toolbar.settings();
 
         expect(modalPushSpy).toHaveBeenCalledWith(
             EModals.Settings,
@@ -214,8 +268,9 @@ describe("Given a main page component", () => {
         modalPushSpy.and.returnValue(observableMock);
         setLanguageSpy.calls.reset();
 
-        component.settings();
+        toolbar.settings();
         observableMock.next(ELanguages.Klingon);
+        fixture.detectChanges();
 
         expect(setLanguageSpy).toHaveBeenCalledWith(ELanguages.Klingon);
     });
@@ -225,8 +280,9 @@ describe("Given a main page component", () => {
         modalPushSpy.and.returnValue(observableMock);
         setLanguageSpy.calls.reset();
 
-        component.settings();
+        toolbar.settings();
         observableMock.error({});
+        fixture.detectChanges();
 
         expect(setLanguageSpy).not.toHaveBeenCalled();
     });
@@ -237,7 +293,7 @@ describe("Given a main page component", () => {
         closeMessageSpy.and.returnValue(of(closeMessage));
         const expectedDialogMessage: IMessageParam = { message: closeMessage };
 
-        component.close();
+        toolbar.close();
 
         expect(modalPushSpy).toHaveBeenCalledWith(EModals.Confimation, expectedDialogMessage);
     });
@@ -246,7 +302,7 @@ describe("Given a main page component", () => {
         modalPushSpy.and.returnValue(of({}));
         closeMessageSpy.and.returnValue(of(""));
 
-        component.close();
+        toolbar.close();
 
         expect(closeSpy).toHaveBeenCalled();
     });
@@ -256,8 +312,9 @@ describe("Given a main page component", () => {
         modalPushSpy.and.returnValue(observableMock);
         closeMessageSpy.and.returnValue(of(""));
 
-        component.close();
+        toolbar.close();
         observableMock.error({});
+        fixture.detectChanges();
 
         expect(closeSpy).not.toHaveBeenCalled();
     });
