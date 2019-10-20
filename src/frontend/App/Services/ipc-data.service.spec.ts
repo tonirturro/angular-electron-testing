5480ca6cdbb933ee0776882698d3e68bc96dca9b
Antonio Garcia <a.garcia@hp.com>
2019-10-20 22:04:20 +0200

    Add more api's

--- src/frontend/App/Services/ipc-data.service.spec.ts
+++ src/frontend/App/Services/ipc-data.service.spec.ts
@@ -1,7 +1,8 @@
 import { fakeAsync, tick } from "@angular/core/testing";
 import { IpcMain, IpcMainEvent, IpcRenderer } from "electron";
 import * as ipcMock from "electron-ipc-mock";
-import { IPage } from "../../../common/rest";
+import { PageFields } from "../../../common/model";
+import { IDevice, IPage, ISelectableOption } from "../../../common/rest";
 import { DataService } from "./ipc-data.service";
 
 describe("Given a data service", () => {
@@ -14,6 +15,14 @@ describe("Given a data service", () => {
         pageSize: "2",
         printQuality: "3",
     }];
+    const expectedDevices: IDevice[] = [{
+        id: 1,
+        name: "Device 2"
+    }];
+    const devicePageOptionsResponse: ISelectableOption[] = [
+        { value: "0", label: "label0 "}
+    ];
+    const ExpectedDeviceId = 1;
     let service: DataService;
     let ipcMain: IpcMain;
     let ipcRenderer: IpcRenderer;
@@ -32,11 +41,11 @@ describe("Given a data service", () => {
         expect(service).toBeTruthy();
     });
 
-    it("When the pages are not yet fetched it sould retrive an empty array", () => {
+    it("When the pages are not yet fetched they should retrive an empty array", () => {
         expect(service.pages.length).toEqual(0);
     });
 
-    it("When the pages are fetched it should be retrieved", fakeAsync(() => {
+    it("When the pages are fetched they should be retrieved", fakeAsync(() => {
         ipcMain.once("pages:get", (event: IpcMainEvent) => {
             event.sender.send("pages:get", expectedPages);
         });
@@ -48,4 +57,58 @@ describe("Given a data service", () => {
         expect(initialPages.length).toEqual(0);
         expect(finalPages).toEqual(expectedPages);
     }));
+
+    it("When the devices are not yet fetched they should retrive an empty array", () => {
+        expect(service.devices.length).toEqual(0);
+    });
+
+    it("When the devices are fetched they should be retrived", fakeAsync(() => {
+        ipcMain.once("devices:get", (event: IpcMainEvent) => {
+            event.sender.send("devices:get", expectedDevices);
+        });
+
+        const initialDevices = service.devices;
+        tick();
+        const finalDevices = service.devices;
+
+        expect(initialDevices.length).toEqual(0);
+        expect(finalDevices).toEqual(expectedDevices);
+    }));
+
+    it("Can read device page options", fakeAsync((done) => {
+        ipcMain.once("devices:capabilities", (event: IpcMainEvent) => {
+            event.sender.send("devices:capabilities", devicePageOptionsResponse);
+        });
+
+        service.getCapabilities(PageFields.PageSize).subscribe((capabilities) => {
+            expect(capabilities).toEqual(devicePageOptionsResponse);
+            done();
+        });
+
+        tick();
+    }));
+
+    it("Can cache device page options", fakeAsync((done) => {
+        ipcMain.once("devices:capabilities", (event: IpcMainEvent) => {
+            event.sender.send("devices:capabilities", devicePageOptionsResponse);
+        });
+
+        service.getCapabilities(PageFields.PageSize).subscribe();
+
+        tick();
+
+        service.getCapabilities(PageFields.PageSize).subscribe((capabilities) => {
+            expect(capabilities).toEqual(devicePageOptionsResponse);
+            done();
+        });
+    }));
+
+    it("Can add pages", (done) => {
+        ipcMain.once("pages:add", (event: IpcMainEvent, deviceId: number) => {
+            expect(deviceId).toEqual(ExpectedDeviceId);
+            done();
+        });
+
+        service.addNewPage(ExpectedDeviceId);
+    });
 });
--- src/frontend/App/Services/ipc-data.service.ts
+++ src/frontend/App/Services/ipc-data.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@angular/core";
 import { IpcRenderer } from "electron";
-import { Observable } from "rxjs";
+import { Observable, of, Subject } from "rxjs";
 import { IDevice, IPage, ISelectableOption } from "../../../common/rest";
 import { IDataService } from "./definitions";
+
+interface ICapabilitiesDictionary {
+    [key: string]: ISelectableOption[];
+}
 
 @Injectable()
 export class DataService implements IDataService {
@@ -13,9 +17,20 @@ export class DataService implements IDataService {
     private cachedPages: IPage[];
     private defaultCachedPages: IPage[] = [];
     private isGettingPages = false;
+    private cachedDevices: IDevice[];
+    private defaultCachedDevices: IDevice[] = [];
+    private isGettingDevices = false;
+    private cachedCapabilities: ICapabilitiesDictionary = {};
 
+    /**
+     * Initializes a new instance from the Data class.
+     * @param ipcRenderer the inter process communication with backend
+     */
     constructor(private ipcRenderer: IpcRenderer) {}
 
+    /**
+     * Gets the current cached pages
+     */
     public get pages(): IPage[] {
         if (this.cachedPages) {
             return this.cachedPages;
@@ -29,16 +44,46 @@ export class DataService implements IDataService {
         return this.defaultCachedPages;
     }
 
+    /**
+     * Gets the current cached devices
+     */
     public get devices(): IDevice[] {
-        return [];
+        if (this.cachedDevices) {
+            return this.cachedDevices;
+        }
+
+        if (!this.isGettingDevices) {
+            this.isGettingDevices = true;
+            this.updateDevices();
+        }
+
+        return this.defaultCachedDevices;
     }
 
+    /**
+     * Get the options available for a particular device capability
+     * @param capability the capability to be queried
+     */
     public getCapabilities(capability: string): Observable<ISelectableOption[]> {
-        return new Observable();
+        if (this.cachedCapabilities[capability]) {
+            return of(this.cachedCapabilities[capability]);
+        }
+
+        return this.getCapabilitiesFromModel(capability);
+    }
+
+    /**
+     * Request a new page
+     */
+    public addNewPage(deviceId: number) {
+        this.ipcRenderer.once("pages:add", () => {
+            this.updatePages();
+        });
+
+        this.ipcRenderer.send("pages:add", deviceId);
     }
 
     // tslint:disable: no-empty
-    public addNewPage(deviceId: number) {}
     public addNewDevice(name: string) {}
     public deletePage(idToDelete: number) {}
     public deleteDevice(idToDelete: number) {}
@@ -50,5 +95,26 @@ export class DataService implements IDataService {
             this.cachedPages = pages;
         });
         this.ipcRenderer.send("pages:get");
+    }
+
+    private updateDevices() {
+        this.ipcRenderer.once("devices:get", (event: any, devices: IDevice[]) => {
+            this.cachedDevices = devices;
+        });
+        this.ipcRenderer.send("devices:get");
+    }
+
+    private getCapabilitiesFromModel(capability: string): Observable<ISelectableOption[]> {
+        const capabilitiesResult = new Subject<ISelectableOption[]>();
+
+        this.ipcRenderer.once("devices:capabilities", (event: any, options: ISelectableOption[]) => {
+            this.cachedCapabilities[capability] = options;
+            capabilitiesResult.next(options);
+            capabilitiesResult.complete();
+        });
+
+        // this.ipcRenderer.send("devices:capabilities", capability);
+
+        return capabilitiesResult;
     }
 }
